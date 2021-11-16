5506e80fb3ec448c2751131fc5a5e9b1fdac4165
oppong <michaeloppong123@gmail.com>
2021-11-15 21:33:30 -0500

    setup redux and working on login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import { store } from "./redux/store";
 
-function App() {
+import { Welcome, Dashboard } from "./screens";
+
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Welcome /> },
+    { path: "dashboard", element: <Dashboard /> },
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <App />
+    </Router>
   );
-}
+};
 
-export default App;
+const AppWithRedux = () => {
+  return (
+    <Provider store={store}>
+      <AppWrapper />
+    </Provider>
+  );
+};
+export default AppWithRedux;
