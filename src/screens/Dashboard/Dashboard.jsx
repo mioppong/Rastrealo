559dffb0b2173cb9d3810ac7b42488d5a5249bea
oppong <michaeloppong123@gmail.com>
2021-11-16 22:05:01 -0500

    working on main dashboard, just a button

--- src/screens/Dashboard/Dashboard.jsx
+++ src/screens/Dashboard/Dashboard.jsx
@@ -1,8 +1,38 @@
-import React, { Component } from "react";
+import { Button, Modal, Paper } from "@mui/material";
+import React, { useState } from "react";
+import "./DashboardStyle.css";
 
-class Dashboard extends Component {
-  render() {
-    return <div>ayee</div>;
-  }
-}
+const Dashboard = () => {
+  const [state, setstate] = useState(false);
+  return (
+    <div style={{ flexDirection: "row", display: "flex" }}>
+      <Paper
+        elevation={10}
+        style={{ width: "25vh", height: "100vh", background: "pink" }}
+      >
+        <Button
+          onClick={() => setstate(true)}
+          style={{ width: "50px", height: "50px", backgroundColor: "red" }}
+        />
+        <Modal
+          open={state}
+          style={{ width: "50px", height: "50px", backgroundColor: "blue" }}
+        >
+          <Paper style={{ width: "50px", height: "50px" }} />
+        </Modal>
+      </Paper>
+
+      <Paper
+        elevation={10}
+        style={{
+          width: "100%",
+          height: "100vh",
+          marginLeft: "25px",
+        }}
+      >
+        <Button children="ADD" />
+      </Paper>
+    </div>
+  );
+};
 export default Dashboard;
